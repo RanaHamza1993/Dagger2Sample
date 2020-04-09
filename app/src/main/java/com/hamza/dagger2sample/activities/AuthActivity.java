ab5a0bcf49b16807f92274020f603e36e269d7fe
hackerrank <me@hackerrank.com>
2020-04-09 15:26:39 +0500

    push

--- app/src/main/java/com/hamza/dagger2sample/activities/AuthActivity.java
+++ app/src/main/java/com/hamza/dagger2sample/activities/AuthActivity.java
@@ -2,13 +2,16 @@ package com.hamza.dagger2sample.activities;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.RequestManager;
 import com.hamza.dagger2sample.R;
 import com.hamza.dagger2sample.databinding.ActivityAuthBinding;
+import com.hamza.dagger2sample.models.User;
 import com.hamza.dagger2sample.viewmodels.AuthViewModel;
 import com.hamza.dagger2sample.viewmodels.ViewModelProviderFactory;
 
@@ -35,12 +38,32 @@ public class AuthActivity extends DaggerAppCompatActivity {
         setContentView(binding.getRoot());
         viewModel= new ViewModelProvider(this,providerFactory).get(AuthViewModel.class);
         setLogo();
-       // Log.d(TAG, something);
-
-
+        setListeners();
+        subscribeObservers();
+    }
+    private void setListeners(){
+        binding.loginButton.setOnClickListener(v->{
+            attemptLogin();
+        });
     }
     private void setLogo(){
         requestManager.load(logo)
                 .into(binding.loginLogo);
+    }
+    private void subscribeObservers(){
+        viewModel.getUserLiveData().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if(user!=null){
+                    Log.d(TAG, "onChanged: "+user.getEmail());
+                }
+            }
+        });
+    }
+    private void attemptLogin(){
+        if(TextUtils.isEmpty(binding.userIdInput.getText().toString())){
+            return;
+        }
+        viewModel.autheticateWithId(Integer.parseInt(binding.userIdInput.getText().toString()));
     }
 }
--- app/src/main/java/com/hamza/dagger2sample/viewmodels/AuthViewModel.java
+++ app/src/main/java/com/hamza/dagger2sample/viewmodels/AuthViewModel.java
@@ -2,14 +2,47 @@ package com.hamza.dagger2sample.viewmodels;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
+
+import com.hamza.dagger2sample.models.User;
+import com.hamza.dagger2sample.network.AuthApi;
 
 import javax.inject.Inject;
 
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class AuthViewModel extends ViewModel {
+    private final AuthApi authApi;
     private static final String TAG = "AuthViewModel";
+    private MediatorLiveData mediatorLiveData = new MediatorLiveData<User>();
+
     @Inject
-    public AuthViewModel(){
+    public AuthViewModel(AuthApi authApi) {
         Log.d(TAG, "AuthViewModel: ViewModel is working.....");
+        this.authApi = authApi;
+
+    }
+    public void autheticateWithId(int id){
+        final LiveData<User> source= LiveDataReactiveStreams.fromPublisher(
+          authApi.getUser(id).
+          subscribeOn(Schedulers.io())
+        );
+        mediatorLiveData.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                mediatorLiveData.setValue(user);
+                mediatorLiveData.removeSource(source);
+
+            }
+        });
+    }
+    public LiveData<User> getUserLiveData(){
+        return mediatorLiveData;
     }
 }

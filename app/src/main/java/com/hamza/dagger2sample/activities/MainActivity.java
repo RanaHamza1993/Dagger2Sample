9ad0b6d19d75abbe474d1843847cda83334e2050
hackerrank <me@hackerrank.com>
2020-04-10 16:47:40 +0500

    push

--- app/src/main/java/com/hamza/dagger2sample/activities/MainActivity.java
+++ app/src/main/java/com/hamza/dagger2sample/activities/MainActivity.java
@@ -2,36 +2,51 @@ package com.hamza.dagger2sample.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 
+import com.google.android.material.navigation.NavigationView;
 import com.hamza.dagger2sample.R;
 import com.hamza.dagger2sample.app.BaseApplication;
+import com.hamza.dagger2sample.databinding.ActivityMainBinding;
 import com.hamza.dagger2sample.di.components.DaggerAppComponent;
 import com.hamza.dagger2sample.fragments.PostsFragment;
 import com.hamza.dagger2sample.fragments.ProfileFragment;
 
 import javax.inject.Inject;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-      //  showToast(TAG);
-        testFragment();
+        // setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //  showToast(TAG);
+       // testFragment();
+        init();
     }
 
-    private void testFragment(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame,new PostsFragment()).commit();
+    private void testFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, new PostsFragment()).commit();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -41,13 +56,59 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.logout: {
                 sessionManager.logOut();
                 navAuthScreen();
                 return true;
-            } default:
+            }
+            case android.R.id.home: {
+                if (binding.drawerLayout.isDrawerOpen(Gravity.START)) {
+                    binding.drawerLayout.closeDrawer(Gravity.START);
+                    return true;
+                } else
+                    return false;
+            }
+            default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile: {
+                NavOptions navOptions = new NavOptions.Builder().setPopUpTo(R.id.main, true)
+                        .build();
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(
+                        R.id.profileFragment,
+                        null,
+                        navOptions);
+                break;
+            }
+            case R.id.nav_posts: {
+                if(isValidDestination(R.id.nav_posts))
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsFragment);
+                break;
+            }
+        }
+        item.setCheckable(true);
+        binding.drawerLayout.closeDrawer(Gravity.START);
+        return false;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), binding.drawerLayout);
+    }
+    private boolean isValidDestination(int destination){
+        return destination!=Navigation.findNavController(this,R.id.nav_host_fragment).getCurrentDestination().getId();
+    }
+
+    private void init() {
+        NavController navHostController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navHostController, binding.drawerLayout);
+        NavigationUI.setupWithNavController(binding.navView, navHostController);
+        binding.navView.setNavigationItemSelectedListener(this);
     }
 }
--- app/src/main/java/com/hamza/dagger2sample/fragments/PostsFragment.java
+++ app/src/main/java/com/hamza/dagger2sample/fragments/PostsFragment.java
@@ -1,43 +1,30 @@
 package com.hamza.dagger2sample.fragments;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.hamza.dagger2sample.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import com.hamza.dagger2sample.adapters.PostsRecyclerAdapter;
 import com.hamza.dagger2sample.databinding.FragmentPostsBinding;
-import com.hamza.dagger2sample.databinding.FragmentProfileBinding;
 import com.hamza.dagger2sample.models.Post;
 import com.hamza.dagger2sample.utils.ApiResource;
 import com.hamza.dagger2sample.utils.VerticalSpacingItemDecoration;
 import com.hamza.dagger2sample.viewmodels.PostsViewModel;
 import com.hamza.dagger2sample.viewmodels.ViewModelProviderFactory;
-
 import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
-
 import javax.inject.Inject;
-
 import dagger.android.support.DaggerFragment;
 
 public class PostsFragment extends DaggerFragment {
     private FragmentPostsBinding binding;
     private static final String TAG = "PostsFragment";
-   private PostsViewModel viewModel;
+    private PostsViewModel viewModel;
     @Inject
     ViewModelProviderFactory factory;
     @Inject
@@ -50,7 +37,7 @@ public class PostsFragment extends DaggerFragment {
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       binding= FragmentPostsBinding.inflate(inflater,container,false);
+        binding = FragmentPostsBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
@@ -61,40 +48,43 @@ public class PostsFragment extends DaggerFragment {
         initRecyclerView();
         subscribeObservers();
     }
+
     private void subscribeObservers() {
         viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<ApiResource<List<Post>>>() {
             @Override
             public void onChanged(ApiResource<List<Post>> listApiResource) {
-                if(listApiResource!=null) {
+                if (listApiResource != null) {
                     switch (listApiResource.status) {
-                        case LOADING:{
+                        case LOADING: {
                             Log.d(TAG, "onChanged: Loading....");
                             break;
                         }
-                        case SUCCESS:{
+                        case SUCCESS: {
                             Log.d(TAG, "onChanged: Success");
                             postsRecyclerAdapter.setPosts(listApiResource.data);
                             break;
 
                         }
-                        case ERROR:{
+                        case ERROR: {
                             Log.d(TAG, "onChanged: Error");
                             break;
 
                         }
-                 //       Log.d(TAG, "onChanged: " + listApiResource.data);
+                        //       Log.d(TAG, "onChanged: " + listApiResource.data);
 
                     }
                 }
             }
         });
     }
+
     @Override
     public void onDestroy() {
-        binding=null;
+        binding = null;
         super.onDestroy();
     }
-    private void initRecyclerView(){
+
+    private void initRecyclerView() {
         binding.recyclerView.setLayoutManager(layoutManager);
         binding.recyclerView.addItemDecoration(verticalSpacingItemDecoration);
         binding.recyclerView.setAdapter(postsRecyclerAdapter);
